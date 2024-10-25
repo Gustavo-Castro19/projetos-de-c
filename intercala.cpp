cba03abf8f95d3ceacacbdc5f4cc8844fbcf0b0b
Gustavo <decastrogustavo19@proton.me>
2024-10-25 19:00:30 -0200

    melhora na lógica de ordenação

--- intercala.cpp
+++ intercala.cpp
@@ -1,31 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	int vetor1[5]={0},vetor2[5]={0},VetorR[5]={0},i;
-	printf("digite 5 numeros em ordem\n");
-	for(i=0;i<5;i++){
-		scanf("%d",&vetor1[i]);
-	}
-	printf("digite outros 5 numeros\n");
-	for(i=0;i<5;i++){
-		scanf("%d",&vetor2[i]);
-	}
-	int compara=0;
-	for(i=0;i<5;i++){
-		if(vetor1[i]==vetor2[i] || vetor1[i]<vetor2[i]){
-			compara=vetor1[i];
-			
+void bubblesort(int vt[], int tamanho);// uma função de bubblesort
+
+int main() {
+    int vet1[5], vet2[5], vetR[10], i;
+
+
+    printf("Digite o 1 vetor:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &vet1[i]);
+    }
+    
+    printf("Digite o 2 vetor:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &vet2[i]);
+    }
+
+    // junta os dois vetores em 1 só
+    for (i = 0; i < 5; i++) {
+        vetR[i] = vet1[i];        // copia os valores do vetor 1 nas posições  0 a 4
+        vetR[i + 5] = vet2[i];    // copia os valores do vetor 2 nas posições  5 a 9
+    }
+
+    // ordena o vetor resultante
+    bubblesort(vetR, 10);
+
+    //mostra o resultado
+    printf("As duas listas juntas formam:\n");
+    for (i = 0; i < 10; i++) {
+    	if(i!=0 && vetR[i]==vetR[i-1]){ //condição para verificar duplicadas
+        	continue;
 		}
-        else if(vetor1[i]>vetor2[i]){
-        	compara=vetor2[i];
-		}
-		VetorR[i]=compara;
-	}
-	printf("vetor resultante: ");
-	for(i=0;i<5;i++){
-		printf("%d",VetorR[i]);
-		if(i<4) printf(",");
-	}
-	return 0; 
+        printf("%d ", vetR[i]);                             
+    }
+    printf("\n"); 
+    return 0;
 }
+
+void bubblesort(int vt[], int tamanho) {
+    int i, j, aux;
+    for (i = 0; i < tamanho - 1; i++) {
+        for (j = 0; j < tamanho - i - 1; j++) {
+            if (vt[j] > vt[j + 1]) {
+                aux = vt[j];
+                vt[j] = vt[j + 1];
+                vt[j + 1] = aux;
+            }
+        }
+    }
+}
+
+
+
